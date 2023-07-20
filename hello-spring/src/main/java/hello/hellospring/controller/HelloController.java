44af6dcee7edef6e6a9f4650a218b454f5658887
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-07-20 13:22:27 +0900

    MVC 방식을 사용한 컨트롤러, 뷰 파일 추가

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
@@ -11,5 +12,13 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    // 외부에서 직접 받기
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        // 외부에서 받아온 name을 name 키값에 넘겨 주기
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
