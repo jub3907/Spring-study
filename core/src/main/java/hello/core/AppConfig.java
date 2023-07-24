4ce7f2b9a68d2ae6b2d8058aeabc16c4d7c08729
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-07-24 16:21:34 +0900

    현재까지의 예제 스프링으로 전환

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // Application의 환경 구성을 Config에서 전담.
+// @Configuration : 설정 정보
+@Configuration
 public class AppConfig {
 
+    // @Bean -> 스프링 컨테이너에 등록됨.
     // Repository는 Memory로 할 것이다.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // Discount Policy는 Fix를 사용한다.
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
 
     // 멤버 서비스에선, MemberServiceImpl을 사용하겠다.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // 오더 서비스에선 OrderServiceImpl을 사용하겠다.
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
