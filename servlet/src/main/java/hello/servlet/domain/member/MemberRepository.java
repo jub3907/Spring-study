c9f668259a27aa4c4bc8765746851c9981485b64
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-08-08 13:51:00 +0900

    서블릿과 JSP를 사용해 회원관리예제 구현

---
+++ servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,41 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않기 때문에, 실무에선 ConcurrentHashMap, AtomicLong 사용을 고려해야 한다.
+ */
+public class MemberRepository {
+
+    private Map<Long, Member> store = new HashMap<>();
+    // static이기 때문에, 전역으로 한개만 존재.
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
