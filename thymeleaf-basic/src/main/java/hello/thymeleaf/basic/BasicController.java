f934db26fd11310fabe48a9539d8f6bb146049c5
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-08-14 11:48:37 +0900

    반복 예제 추가

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -63,13 +63,13 @@ public class BasicController {
         }
     }
 
-    @GetMapping("date")
+    @GetMapping("/date")
     public String date(Model model) {
         model.addAttribute("localDateTime", LocalDateTime.now());
         return "basic/date";
     }
 
-    @GetMapping("link")
+    @GetMapping("/link")
     public String link(Model model) {
         model.addAttribute("param1", "data1");
         model.addAttribute("param2", "data2");
@@ -77,13 +77,13 @@ public class BasicController {
         return "basic/link";
     }
 
-    @GetMapping("literal")
+    @GetMapping("/literal")
     public String literal(Model model) {
         model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
 
-    @GetMapping("operation")
+    @GetMapping("/operation")
     public String operation(Model model) {
         model.addAttribute("nullData", null);
         model.addAttribute("data", "Spring");
@@ -91,10 +91,27 @@ public class BasicController {
         return "basic/operation";
     }
 
-    @GetMapping("attribute")
+    @GetMapping("/attribute")
     public String attribute() {
         return "basic/attribute";
     }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+    }
+
 
     @Data
     static class User {
