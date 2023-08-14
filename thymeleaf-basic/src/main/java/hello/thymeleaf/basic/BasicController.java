0cb64b85d7e8c0353b44a9f44ea9ced2697d709f
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-08-14 10:51:56 +0900

    Spring-EL 내용 추가

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -27,5 +27,36 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 20);
+        User userB = new User("userB", 30);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
 
 }
