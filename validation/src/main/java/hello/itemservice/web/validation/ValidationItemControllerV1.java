8bef3b97d1833eec797895c7d7cbca63d9feb728
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-08-17 01:48:41 +0900

    프로젝트 검증 V1 추가

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,13 +3,18 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -38,7 +43,45 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 오류 결과 보관
+        Map<String, String> errors = new HashMap<>();
+
+        // 검증 로직
+            // 아이템 이름이 없다면
+        if (!StringUtils.hasText((item.getItemName()))) {
+            // itemName 오류 발생.
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "상품 가격은 1,000 ~ 1,000,000까지 허용합니다.");
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "상품 수량은 최대 9999개까지 허용합니다.");
+        }
+
+        // 가격 * 수량의 합은 10000 이상
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getQuantity() * item.getPrice();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 = " + resultPrice);
+            }
+        }
+
+
+        // 검증 실패 시, 다시 입력 폼으로 이동.
+        // 부정의 부정은 읽기 힘드므로, 리팩토링 필요.
+        if (!errors.isEmpty()) {
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        // 성공 로직
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
