06e58f2ff05d406424587e6e389fa69424b7a4de
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-08-23 14:09:36 +0900

    API의 예외 처리 방법 예제 추가

---
+++ exception/src/main/java/hello/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,11 @@
+package hello.exception.exhandler.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExControllerAdvice {
+
+
+}
