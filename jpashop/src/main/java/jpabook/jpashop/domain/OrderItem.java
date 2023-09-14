61e9ac1d4765588c581465f6dd68d1e746a6c380
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-09-14 13:47:52 +0900

    엔티티 구현

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id") // 연관관계 주인을 order 테이블로 설정
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+}
