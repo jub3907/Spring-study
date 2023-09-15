698d4b16d1eee639aef2101fba513ba8cf3f1fae
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-09-15 13:13:27 +0900

    회원 도메인 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)  // 데이터 변경 -> transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // readonly false 설정.
+    public Long join(Member member) {
+        // 이름이 중복인 멤버가 존재하면 오류 발생
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     */
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        // Exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+}
