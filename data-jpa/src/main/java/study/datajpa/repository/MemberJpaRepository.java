b04a2c3946d567fa5021ac22dccedcf974b6f39c
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-09-27 14:00:05 +0900

    공통 인터페이스 기능 구현

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -7,6 +7,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -14,12 +16,32 @@ public class MemberJpaRepository {
     @PersistenceContext
     private EntityManager em;
 
+    // Create
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
+    // Read
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    // Delete
+    public void delete(Member member) {
+        em.remove(member);
     }
 }
