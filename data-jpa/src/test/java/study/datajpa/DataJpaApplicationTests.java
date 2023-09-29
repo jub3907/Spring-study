2088add5df3d76d2f6b4d78ca90477f3f560c458
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-09-29 16:45:37 +0900

    확장 기능 구현

--- data-jpa/src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ data-jpa/src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -2,6 +2,7 @@ package study.datajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
