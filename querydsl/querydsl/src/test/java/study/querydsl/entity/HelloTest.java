1698db85114d9605c230b5ef4468faa86cc1d79f
SeungjuLee <58246682+jub3907@users.noreply.github.com>
2023-10-01 15:38:24 +0900

    querydsl 기본 문법

---
+++ querydsl/querydsl/src/test/java/study/querydsl/entity/HelloTest.java
@@ -0,0 +1,37 @@
+package study.querydsl.entity;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class HelloTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void hello() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+    }
+}
